f2147f358a049a7f205d1feee3a6411508b5b479
Jeannie She <jeanshe@mit.edu>
2026-06-23 17:23:48 -0700

    use built in wandb <> sb3 integration for tracking

--- notebooks/jeannie/wandb_stablebaselines.py
+++ notebooks/jeannie/wandb_stablebaselines.py
@@ -1,24 +1,27 @@
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
+# from stable_baselines3.common.callbacks import BaseCallback
+from wandb.integration.sb3 import WandbCallback
 import wandb
 
-class WandbCallback(BaseCallback):
-    def __init__(self, verbose=0):
-        super(WandbCallback, self).__init__(verbose)
+# class WandbCallback(BaseCallback):
+#     def __init__(self, verbose=0):
+#         super(WandbCallback, self).__init__(verbose)
 
-    def _on_step(self) -> bool:
-        # log training progress on each RL step natively
-        wandb.log(
-            {"train/reward": self.locals["rewards"][0]},
-            step = self.num_timesteps
-            )
-        
+    # def _on_step(self) -> bool:
+    #     # log training progress on each RL step natively
+    #     wandb.log(
+    #         {"train/reward": self.locals["rewards"][0]},
+    #         step = self.num_timesteps
+    #         )
+    #     return True
+
 # initialize wandb
 run = wandb.init(
     entity="goodarzilab",
     project="relearn",
+    sync_tensorboard=True
 )
 
 # train agent
-model = PPO("MlpPolicy", "CartPole-v1", verbose=1)
-model.learn(total_timesteps=1000, callback=WandbCallback())
+model = PPO("MlpPolicy", "CartPole-v1", verbose=1, tensorboard_log=f"runs/{run.id}")
+model.learn(total_timesteps=20000, callback=WandbCallback())
