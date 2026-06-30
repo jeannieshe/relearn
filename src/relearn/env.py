b749078120a4f7465fce2427c126828799382b01
Jeannie She <jeanshe@mit.edu>
2026-06-29 17:37:06 -0700

    progress check on env dev

--- src/relearn/env.py
+++ src/relearn/env.py
@@ -16,18 +16,46 @@ class RelearnChemicalEnv(gym.Env):
         self.pert_matrix = torch.stack(list(pert_map.values())) # shape: (1138, 1138)
         self.action_space = gym.spaces.Discrete(len(self.drug_list))
 
-        # # define what the agent can observe
-        # with open("../../notebooks/jeannie/ST-HVG-Tahoe/fewshot/state_generalization_X_hvg/cell_type_onehot_map.pkl", 'rb') as file:
-        #     self.cell_type_map = pickle.load(file)
+        # define what the agent can observe
+        # choose one fixed cell type first
 
-        # # pass in the cell state and the cell type
-        # # concatenate and flatten into one vector
-        # self.observation_space = gym.spaces.Box(
-        #     low=0, 
-        #     high=np.inf,
-        #     shape=(2000 + len(self.cell_type_map),),
-        #     dtype=np.float32
-        # )
+        # pass in the cell state
+        self.observation_space = gym.spaces.Box(
+            low=0, 
+            high=np.inf,
+            shape=(2000,),
+            dtype=np.float32
+        )
+        # begin with a neutral cell state
+        # TODO: this should be biologically meaningful instead of just zeroes
+        self.initial_cell_state = np.array(np.zeros(shape=(2000,), dtype=np.float32))
+        self._cell_state = self.initial_cell_state
 
+        # define the apoptosis classifier
+        self.apoptosis_predictor = pass
+    
     def _get_obs(self):
-        pass
+        return {
+            "cell_state": self._cell_state,
+        }
+
+    def _get_info(self):
+        return {
+            "apoptosis score": self.apoptosis_predictor(self._cell_state),
+        }
+
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        # seed the rng
+        super().reset(seed=seed)
+
+        # reset the cell state
+        self._cell_state = self.initial_cell_state
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, info
+
+    def step(self, action):
+        # begin with an uninformed agent, take a random action
+        
